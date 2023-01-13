dc3aa237df429bfd3c0f7759bbfb14f956587d65
msmolkin <45619570+msmolkin@users.noreply.github.com>
2023-01-13 15:17:08 -0800

    Create greenhouse_job_applications.js
    2023-01-04

---
+++ greenhouse_job_applications.js
@@ -0,0 +1,7 @@
+$("#first_name").val("Michael");
+$("#last_name").val("Smolkin");
+$("#email").val("michael@smolkin.org");
+$("#phone").val("4158917599");
+$("#job_application_location").val("San Francisco, California, United States");
+$("#job_application_answers_attributes_0_text_value").val("https://www.linkedin.com/in/msmolkin");
+$("#job_application_answers_attributes_1_text_value").val("https://michael.smolkin.org/projects-grid-cards.html"); 
